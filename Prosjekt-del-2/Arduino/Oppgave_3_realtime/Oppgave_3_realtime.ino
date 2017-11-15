5f69b11b081992fc9b9f1b6b48435e8d9a0a24c3
Fredrik Kartevoll <Fredrikkartevoll@gmail.com>
2017-11-15 08:59:25 +0100

    vetikkeeiganghvasomharskjedd

--- Prosjekt-del-2/Arduino/Oppgave_3_realtime/Oppgave_3_realtime.ino
+++ Prosjekt-del-2/Arduino/Oppgave_3_realtime/Oppgave_3_realtime.ino
@@ -2,12 +2,12 @@
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 int ledPin = 13;  // LED connected to digital pin 13
-const double maxg = 2.0;
-const double gScale = 32768.0 / maxg;
-double verdier[10];
-double differanse = 0.0;
-double totDiff = 0.0;
-double accZ = 0.0;
+const float maxg = 2.0;
+const float gScale = 32768.0 / maxg;
+float values[10];
+float diff = 0.0;
+float totDiff = 0.0;
+float accZ = 0.0;
 
 void setup(){
   Wire.begin();
@@ -28,31 +28,33 @@ void loop()
     Wire.requestFrom(MPU_addr,2,true);  // request a total of 14 registers
     AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
 
-    accZ = static_cast<double>(AcZ);
+    accZ = static_cast<float>(AcZ);
 
-    verdier[9-i] = abs(accZ);
+    values[9] = (accZ + gScale);
+    values[i] = (accZ + gScale);
 
-    if (i == 9)
+    if (i == 0)
     {
-      differanse = abs(verdier[9]-verdier[0]);
+      diff = (values[0]-values[9]);
     }
     else
     {
-      differanse = abs(verdier[8-i]-verdier[9-i]);
+      diff = (values[i]-values[i-1]);
     }
 
-    totDiff = totDiff + differanse;
+    totDiff = totDiff + diff;
     
     Serial.println(totDiff / gScale); //  Can be enabled to read change in gforces for Z direction
     
-    if ((totDiff / gScale) > 1.0)
+    if ((abs(totDiff / gScale) >= 1.0) )
     {
       digitalWrite(ledPin, !digitalRead(ledPin));  // toggle state
       totDiff = 0.0;
-      Serial.print("hei");
+      diff = 0.0;
+      Serial.println("OVERLOAD");
       break;
-    delay(100);
     }
+    delay(100);
   }
   totDiff = 0.0;
 }
